c9a42e1040a896be85dd771dc0b673a353609baa
berberman <1793913507@qq.com>
2019-07-19 13:07:18 +0800

    fix nojekyll

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -3,4 +3,4 @@
 mkdir res
 touch build/v5-chinese/html/.nojekyll
 echo 'pros.mechdancer.org'>build/v5-chinese/html/CNAME
-cp -rf ./build/v5-chinese/html/* ./res/
+cp -rfa ./build/v5-chinese/html/* ./res/
